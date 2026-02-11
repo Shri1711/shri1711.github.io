87c8c8043732f6a96850d5e716e6ed36dc3e6dd5
Shri1711 <117707176+Shri1711@users.noreply.github.com>
2026-02-11 10:30:53 +0530

    Scroll Reveal

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// Typing animation
-const text = ".NET Developer | C# | ASP.NET Core | Angular | SQL Server";
-let i = 0;
-const speed = 70;
-const typing = document.querySelector(".typing");
-
-function typeEffect() {
-  if (i < text.length) {
-    typing.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeEffect, speed);
-  }
-}
-typeEffect();
-
-// Scroll reveal
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
